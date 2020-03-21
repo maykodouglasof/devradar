0a827199d7348f762831a3c49f3f3fd660b1e212
maykodouglasof <maykodouglasbr@outlook.com>
2020-03-21 16:55:55 -0300

    Atualização

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from 'socket.io-client';
 
-const socket = socketio('http://192.168.0.104:3333', {
+const socket = socketio('http://192.168.0.106:3333', {
   autoConnect: false,
 });
 
